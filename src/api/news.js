4f2ff37f60fe7cd386ec9e5e5fbffb27bc8d3aeb
VY <youngvan757@gmail.com>
2020-02-11 16:17:58 -0500

    added stuff

---
+++ src/api/news.js
@@ -0,0 +1,25 @@
+import { NEWS_KEY } from "../api/constants";
+import { render } from "@testing-library/react";
+
+export const fetchNews = query => {
+  const ne = document.querySelector(".news");
+  console.log("fetching data with: ", query);
+
+  const endpoint = `https://newsapi.org/v2/everything?q=${query}&apiKey=${NEWS_KEY}`;
+
+  const promise = fetch(endpoint);
+
+  promise
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(data => {
+      ne.textContent = `${data.articles.title}`;
+    })
+
+    .catch(error => {
+      console.log(error);
+    });
+};
